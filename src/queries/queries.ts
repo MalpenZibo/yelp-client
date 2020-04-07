de4c9cbc5124ceb9e28ad941080e6c0520b17417
Simone Camito <simone.camito@buildo.io>
2020-04-07 17:08:26 +0200

    change from queryStrict to queryShallow

--- src/queries/queries.ts
+++ src/queries/queries.ts
@@ -1,11 +1,11 @@
-import { queryStrict, expire } from 'avenger';
+import { expire, queryShallow } from 'avenger';
 import { getCurrentView } from 'avenger/lib/browser';
 import * as API from '../API';
 import { locationToView } from '../model';
 
 export const currentView = getCurrentView(locationToView);
 
-export const restaurants = queryStrict(
+export const restaurants = queryShallow(
   (parms: { searchQuery: string; locationQuery: string; radiusQuery: number }) =>
     API.getRestaurants(parms.searchQuery, parms.locationQuery, parms.radiusQuery),
   expire(10000)
