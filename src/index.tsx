0592fc3ab67bd58c672830bbd0c3366984ca760e
Simone Camito <simone.camito@buildo.io>
2020-04-15 13:05:02 +0200

    remove old index and fix on new one

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,36 @@
+import './setup/polyfills';
+// from old index: using require to guarantee they're imported after polyfills are installed
+// need test or use old style
+
+/*
+// using require to guarantee they're imported after polyfills are installed
+// @ts-ignore
+const React = require('react');
+// @ts-ignore
+const ReactDOM = require('react-dom');
+// @ts-ignore
+const App = require('./components/App').default;
+const { IntlProvider } = require('./util/intl');
+// @ts-ignore
+const { loadLocale } = require('./setup/loadLocale');
+// @ts-ignore
+const { serviceWorker } = require('./serviceWorker');
+
+// @ts-ignore
+require('./setup/addDeviceClassName');
+// @ts-ignore
+require('./theme');
+*/
+
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 import { IntlProvider } from './util/intl';
 import { loadLocale } from './setup/loadLocale';
 import ReactDOM from 'react-dom';
 import React from 'react';
+import './setup/addDeviceClassName';
 
-import './theme/index';
+import './theme';
 
 ReactDOM.render(
   <React.StrictMode>
